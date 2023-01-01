e19c5acf35323f877fdbc3400ac9393974bec659
youngmin-woo <bowwow515@hanyang.ac.kr>
2023-01-02 03:25:18 +0900

    Fix: environments variables error

--- src/RouteChangeTracker.js
+++ src/RouteChangeTracker.js
@@ -8,8 +8,8 @@ const RouteChangeTracker = () => {
 
   useEffect(() => {
     // 구글 애널리틱스 운영서버만 적용
-    if (process.env.GOOGLE_ANALYTICS_ID) {
-      ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID);
+    if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
     }
     setInitialized(true);
   }, []);
