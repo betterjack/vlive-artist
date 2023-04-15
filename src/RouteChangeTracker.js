48536b3f4d272f1277d3fc72814c6d81799286ac
Youngmin Woo <bowwow515@hanyang.ac.kr>
2023-04-15 19:30:37 +0900

    Remove: ga id

--- src/RouteChangeTracker.js
+++ src/RouteChangeTracker.js
@@ -7,15 +7,6 @@ const RouteChangeTracker = () => {
   const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    // 구글 애널리틱스 운영서버만 적용
-    console.log(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
-    if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
-      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-    }
-    setInitialized(true);
-  }, []);
-
-  useEffect(() => {
     if (initialized) {
       ReactGA.pageview(location.pathname + location.search);
     }
